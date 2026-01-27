f5d3f134341de9995e74887fd824e28eb668c6d7
jihun <79701544+yangjihun@users.noreply.github.com>
2026-01-28 08:39:49 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 520 ms, Memory: 25164 KB -BaekjoonHub

--- 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.java
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.java
@@ -1,27 +1,51 @@
-import java.util.*;
+import java.util.Scanner;
+
+/*
+
+
+*        *
+**      **
+***    ***
+****  ****
+**********
+****  ****
+***    ***
+**      **
+*        *
+
+
+ */
+
+
 public class Main {
-public static void main(String[] args) {
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-		for (int i=1; i<=N; i++) {
-			for (int star = 0; star<i; star++)
+		sc.close();
+		
+		for (int i = 1; i < N; i++) {
+			for (int j = 1; j <= i; j++)
 				System.out.print("*");
-			for (int star = i; star<N; star++) {
-				System.out.print(" ");System.out.print(" ");
-			}
-			for (int star = 0; star<i; star++)
-				System.out.print("*");
-			System.out.println();
-		}
-		for (int i=N-1; i>0; i--) {
-			for (int star = i; star>0; star--)
-				System.out.print("*");
-			for (int star = i; star<N; star++) {
-				System.out.print(" ");System.out.print(" ");
-			}
-			for (int star = 0; star<i; star++)
+			for (int j = 2*(N-i); j>0 ; j--)
+				System.out.print(" ");
+			for (int j = 1; j <= i; j++)
 				System.out.print("*");
 			System.out.println();
 		}
+		
+		for (int i = N; i > 0 ; i--) {  // i = 5 4 3 2 1
+			for (int j = i ; j > 0 ; j--)
+				System.out.print("*");
+			for (int j = 0 ; j < 2*(N-i) ; j++ )
+				System.out.print(" ");
+			for (int j = i ; j > 0 ; j--)
+				System.out.print("*");
+			System.out.println();
+		}
+		
+		
+		
 	}
+
+
 }
