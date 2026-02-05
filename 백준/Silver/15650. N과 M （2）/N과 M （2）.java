b4c305aadab45a11b53d33820eccae34b3ef75e5
jihun <79701544+yangjihun@users.noreply.github.com>
2026-02-05 09:50:25 +0900

    [Silver III] Title: N과 M (2), Time: 204 ms, Memory: 18324 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Main {
+	// 선언
+	static int N;
+	static int M;
+	static boolean[] visited;
+	static int[] arr;
+	
+	static void BT(int depth) {
+		if (depth==M+1) {
+			for (int i=1; i<=M; i++)
+				System.out.print(arr[i]+" ");
+			System.out.println();
+			return;
+		}
+		for (int i=arr[depth-1];i<N;i++) {
+			if (!visited[i]) {
+				visited[i] = true;
+				arr[depth] = i+1;
+				BT(depth+1);
+				visited[i] = false;
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		// 할당
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		visited = new boolean[N];
+		arr = new int[M+1];
+		BT(1);
+	}
+}
