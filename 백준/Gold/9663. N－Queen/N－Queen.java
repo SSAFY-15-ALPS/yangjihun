60eb560d0f8262f3e93e0138a55406bed2265332
jihun <79701544+yangjihun@users.noreply.github.com>
2026-02-11 08:50:45 +0900

    [Gold IV] Title: N-Queen, Time: 6544 ms, Memory: 18000 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,9 +1,9 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main{
 	static int answer = 0;
 	static int N;
-	static boolean[][] visited;
+	static int[] visited;
 	static int[][] arr;
 	
 	static void BT(int depth) {
@@ -14,30 +14,18 @@ public class Main{
 		
 		for (int i=0; i<N;i++) {
 			if (search(depth,i)) {
-				visited[depth][i] = true;
+				visited[depth] = i;
 				BT(depth+1);
-				visited[depth][i] = false;
+				visited[depth] = -1;
 			}
 		}
 	}
 	
 	static boolean search(int i, int j) {
-		boolean check = false;
-		for (int x=0; x<N; x++) {
-			if (visited[i][x]) return check;
-		}
-		for (int y=0; y<N; y++) {
-			if (visited[y][j]) return check;
-		}
-		for (int num=0; num<N;num++) {
-			if (i-num>=0 && i-num<N && j-num>=0 && j-num<N)
-				if(visited[i-num][j-num]) return check;
-			if (i-num>=0 && i-num<N && j+num>=0 && j+num<N)
-				if (visited[i-num][j+num]) return check;
-			if (i+num>=0 && i+num<N && j-num>=0 && j-num<N)
-				if (visited[i+num][j-num]) return check;
-			if (i+num>=0 && i+num<N && j+num>=0 && j+num<N)
-				if (visited[i+num][j+num]) return check;
+		for (int y=0; y<i; y++) {
+			if (visited[y]==j) return false;
+
+			if (Math.abs(i-y) == Math.abs(j - visited[y])) return false;
 		}
 		return true;
 	}
@@ -45,7 +33,8 @@ public class Main{
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		N = sc.nextInt();
-		visited = new boolean[N][N];
+		visited = new int[N];
+		Arrays.fill(visited, -1);
 		BT(0);
 		System.out.println(answer);
 	}
