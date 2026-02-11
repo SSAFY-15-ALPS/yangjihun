dc7d7e2839c854ac24603e8cb384ef1982d7ff92
jihun <79701544+yangjihun@users.noreply.github.com>
2026-02-11 09:06:39 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 720 ms, Memory: 58112 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -13,16 +13,22 @@ public class Main {
 		return Integer.parseInt(st.nextToken());
 	}
 	public static void main(String[] args) throws Exception {
+		StringBuilder sb = new StringBuilder();
 		int N = nextInt();
 		int[][] arr = new int[N][2];
 		for (int i=0; i<N; i++) {
 			arr[i][0] = nextInt();
 			arr[i][1] = nextInt();
 		}
-		Arrays.sort(arr,(a,b) -> Integer.compare(a[1], b[1]));
-		Arrays.sort(arr,(a,b) -> Integer.compare(a[0], b[0]));
+		Arrays.sort(arr,(a,b) -> {
+			if (a[0] != b[0]) return Integer.compare(a[0],  b[0]);
+			return Integer.compare(a[1], b[1]);
+		});
+		
 		for (int i=0; i<N; i++) {
-			System.out.println(arr[i][0] + " " + arr[i][1]);
+			sb.append(arr[i][0] + " " +arr[i][1]);
+			sb.append("\n");
 		}
+		System.out.println(sb.toString());
 	}
 }
