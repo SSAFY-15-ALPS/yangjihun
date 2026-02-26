f2d88652bf9e218420a28dfde45e2b9ccac10191
jihun <79701544+yangjihun@users.noreply.github.com>
2026-02-26 09:53:40 +0900

    [Silver IV] Title: 수들의 합 2, Time: 124 ms, Memory: 14948 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
@@ -1,31 +1,43 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int M = sc.nextInt();
-		
-		int[]A = new int[N];
-		
-		for(int i=0; i<N; i++) {
-			A[i] = sc.nextInt();
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static int[][] map;
+	static boolean[][] visited;
+	static int N;
+	
+	static int nextInt() throws Exception {
+		while (st==null || !st.hasMoreTokens()) {
+			String line = br.readLine();
+			if (line==null) return Integer.MIN_VALUE;
+			st = new StringTokenizer(line);
 		}
-		
-		int count = 0;
-		
-		for(int i=0; i<N; i++) {
-			int sum = 0;
-			for(int j=i; j<N; j++) {
-				sum += A[j];
-				
-				if(sum == M) {
-					count++;
-					break;
-				}
-				else if(sum > M) break;
+		return Integer.parseInt(st.nextToken());
+	}
+	
+	
+	public static void main(String[] args) throws Exception {
+		// 정의
+		int N = nextInt();
+		int M = nextInt();
+		int[] arr = new int[N];
+		int start = 0;
+		int sum = 0;
+		int answer = 0;
+		// 할당
+		for (int i=0; i<N; i++) {
+			arr[i] = nextInt();
+		}
+		// Solution
+		for (int end=0; end<N; end++) {
+			sum+=arr[end];
+			while (sum>M) {
+				sum -= arr[start++];
 			}
+			if (sum==M) answer++;
 		}
-		System.out.println(count);
+		System.out.println(answer);
 	}
 }
