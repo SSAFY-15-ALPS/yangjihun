7c8f4968cb47d7ae32c3bbb8223e974723238e67
jihun <79701544+yangjihun@users.noreply.github.com>
2026-04-14 14:37:39 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 3116 ms, Memory: 245148 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
@@ -0,0 +1,43 @@
+import sys
+
+input = sys.stdin.readline
+
+N,M = map(int,input().split())
+li = []
+parent = [i for i in range(N+1)]
+depth = [0] * (N+1)
+max_value = 0
+
+for i in range(M):
+    a,b,c = map(int,input().split())
+    li.append([a,b,c])
+
+li.sort(key = lambda x : x[2])
+
+def find(i):
+    if parent[i]!=i:
+        parent[i] = find(parent[i])
+    return parent[i]
+
+def union(a,b):
+    root_a = find(a)
+    root_b = find(b)
+    if root_a == root_b:
+        return False
+    if depth[root_a] > depth[root_b]:
+        parent[root_b] = root_a
+    elif depth[root_a] < depth[root_b]:
+        parent[root_a] = root_b
+    else:
+        parent[root_b] = root_a
+        depth[root_a]+=1
+    return True
+
+cost = 0
+
+for a,b,c in li:
+    if union(a,b):
+        cost+=c
+        max_value = c
+
+print(cost - max_value)
